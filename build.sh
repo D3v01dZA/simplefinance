893c5d6dd3902375292fe9e28e438c0d8bb054a1
Christopher Altona <caltona1@gmail.com>
2024-05-18 15:08:33 -0400

    Docker updates

--- build.sh
+++ build.sh
@@ -13,22 +13,15 @@ echo "Building ui"
 npm run build
 
 echo "Copying ui assets"
-cd ../api
-rm -rf ./src/main/resources/static/*
-mkdir -p ./src/main/resources/static
-cp -r ../ui/build/* ./src/main/resources/static/
-cp ../ui/fav.svg ./src/main/resources/static/assets/
-
-echo "Changing database url"
-git checkout ./src/main/resources/application.properties
-sed -i '' 's|database.db|/data/database.db|g' ./src/main/resources/application.properties
-
-echo "Building jar"
-./gradlew build
+cd ../api-rust
+rm -rf ./static/*
+mkdir -p ./static/
+cp -r ../ui/build/* ./static/
+cp ../ui/fav.svg ./static/
 
 echo "Building docker image"
 docker buildx build --platform linux/amd64 --push -t d3v01d/simplefinance:latest .
 
 git checkout ../ui/src/app/serverSlice.ts
-git checkout ./src/main/resources/application.properties
-rm -rf ./src/main/resources/static/*
+rm -rf ./static/*
+git checkout ./static/*
