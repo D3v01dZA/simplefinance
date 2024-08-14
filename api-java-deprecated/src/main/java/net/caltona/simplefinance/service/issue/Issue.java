9eb1b1f0fd3278d1345007bd1a0b7d63994791ed
Christopher Altona <caltona1@gmail.com>
2024-08-13 20:54:27 -0400

    Remove the deprecated java

--- api-java-deprecated/src/main/java/net/caltona/simplefinance/service/issue/Issue.java
+++
@@ -1,9 +0,0 @@
-package net.caltona.simplefinance.service.issue;
-
-import net.caltona.simplefinance.api.model.JIssue;
-
-public interface Issue {
-
-    JIssue json();
-
-}
--- api-java-deprecated/src/main/java/net/caltona/simplefinance/service/issue/IssueType.java
+++
@@ -1,7 +0,0 @@
-package net.caltona.simplefinance.service.issue;
-
-public enum IssueType {
-
-    TRANSFER_WITHOUT_BALANCE;
-
-}
